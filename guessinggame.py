5da9ca082ba8ab691ab2438d74721b6022364b66
Andreas Haug Nygaard <31880331+Andreashn1@users.noreply.github.com>
2020-10-14 18:34:02 +0100

    Add files via upload
    Did some modifications on the descriptions

--- guessinggame.py
+++ guessinggame.py
@@ -1,21 +1,21 @@
 
-secret_word = "giraffe"
+secret_word = "travel"
 guess = ""
 guess_count = 0
-guess_limit = 3
+guess_limit = 5
 out_of_guesses = False
 
 
 while guess != secret_word and not(out_of_guesses):
     if guess_count < guess_limit:
-        guess = input("I am thinking of a secret word. Enter guess: ")
+        guess = input("I am thinking of a secret word that involves a hobby I love. We can explore other countries by doing this activity. Enter a guess: ")
         guess_count += 1
     else:
         out_of_guesses = True
 if out_of_guesses:
-    print("Out of guesses, YOU LOSE!")
+    print("Sorry, you are out of guesses, YOU LOSE!")
 else:
-    print("YOU WIN!")
+    print("COOL! YOU WON THIS CHALLENGE, BUT NOW IS THE SECOND CHALLENGE...")
 
 secret_number = "10"
 guess = ""
@@ -25,11 +25,11 @@ out_of_guesses = False
 
 while guess != secret_number and not(out_of_guesses):
     if guess_count < guess_limit:
-        guess = input("i am thinking of a secret number. Enter a guess: ")
+        guess = input("I am thinking of a secret number. Its between 1 and 20. Enter a guess: ")
         guess_count += 1
     else:
         out_of_guesses = True
 if out_of_guesses:
-    print("Out of guesses, YOU LOSE!")
+    print("Sorry, you are out of guesses, YOU LOSE!")
 else:
-    print("YOU WIN")
+    print("AWESOME! YOU WIN")
